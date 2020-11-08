a1427f3df3f4e8bddc716a88c9fc824e47dc6252
git-rahulrajesh <rahul-rajesh@outlook.com>
2020-11-07 18:01:43 -0800

    Sumeet Commit

--- src/com/zetcode/Board.java
+++ src/com/zetcode/Board.java
@@ -1,5 +1,6 @@
 package com.zetcode;
 
+//Import Statements to draw the game
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -11,12 +12,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+/**
+ @author Hakim Bashe
+ @author Rahul Rajesh
+ @author Sumeet Sara
+ This is the board class, it contains the grid that the snake game is played on
+ The snake moves by the user pressing the arrow keys, and the snake grows if you eat any apples. There are also mines scattered across the board, if you come into contact with any of them, the game ends. The game also ends if the snake touches itself when moving
+ */
 public class Board extends JPanel implements ActionListener {
 
+    // List of private consants
     private final int B_WIDTH = 600;
     private final int B_HEIGHT = 600;
     private final int DOT_SIZE = 10;
@@ -26,10 +36,23 @@ public class Board extends JPanel implements ActionListener {
     private final int x[] = new int[ALL_DOTS];
     private final int y[] = new int[ALL_DOTS];
 
-    private int delay = 100;
+    // List of private variables initalized
+    private int delay = 50;
     private int dots;
     private int apple_x;
     private int apple_y;
+
+    // Landmines initialized
+    private int landmine1_x;
+    private int landmine1_y;
+    private int landmine2_x;
+    private int landmine2_y;
+    private int landmine3_x;
+    private int landmine3_y;
+    private int landmine4_x;
+    private int landmine4_y;
+    private int landmine5_x;
+    private int landmine5_y;
 
     private boolean leftDirection = false;
     private boolean rightDirection = true;
@@ -37,16 +60,19 @@ public class Board extends JPanel implements ActionListener {
     private boolean downDirection = false;
     private boolean inGame = true;
 
+    // Images
     private Timer timer;
     private Image ball;
     private Image apple;
     private Image head;
+    private Image landMine;
 
     public Board() {
-        
+
         initBoard();
     }
-    
+
+    // Initiate the game by creating game window, and loading images of assets
     private void initBoard() {
 
         addKeyListener(new TAdapter());
@@ -58,6 +84,7 @@ public class Board extends JPanel implements ActionListener {
         initGame();
     }
 
+    // Get the images of the snake body, snake food, snake body, and landmines
     private void loadImages() {
 
         ImageIcon iid = new ImageIcon("src/resources/dot.png");
@@ -68,6 +95,10 @@ public class Board extends JPanel implements ActionListener {
 
         ImageIcon iih = new ImageIcon("src/resources/head.png");
         head = iih.getImage();
+
+        // Get the landmine image
+        ImageIcon iim = new ImageIcon(("src/resources/landmines.png"));
+        landMine = iim.getImage();
     }
 
     private void initGame() {
@@ -78,8 +109,9 @@ public class Board extends JPanel implements ActionListener {
             x[z] = 50 - z * 10;
             y[z] = 50;
         }
-        
+
         locateApple();
+        insertMines();
 
         timer = new Timer(delay, this);
         timer.start();
@@ -91,12 +123,17 @@ public class Board extends JPanel implements ActionListener {
 
         doDrawing(g);
     }
-    
-    private void doDrawing(Graphics g) {
-        
-        if (inGame) {
 
+    private void doDrawing(Graphics g) {
+
+        if (inGame) {
             g.drawImage(apple, apple_x, apple_y, this);
+            // draw the landmines
+            g.drawImage(landMine, landmine1_x, landmine1_y, this);
+            g.drawImage(landMine, landmine2_x, landmine2_y, this);
+            g.drawImage(landMine, landmine3_x, landmine3_y, this);
+            g.drawImage(landMine, landmine4_x, landmine4_y, this);
+            g.drawImage(landMine, landmine5_x, landmine5_y, this);
 
             for (int z = 0; z < dots; z++) {
                 if (z == 0) {
@@ -111,11 +148,14 @@ public class Board extends JPanel implements ActionListener {
         } else {
 
             gameOver(g);
-        }        
+        }
     }
 
+
+
+    //Prints the  gameover message and then ends the game afterwards
     private void gameOver(Graphics g) {
-        
+
         String msg = "Game Over";
         Font small = new Font("Helvetica", Font.BOLD, 14);
         FontMetrics metr = getFontMetrics(small);
@@ -125,6 +165,7 @@ public class Board extends JPanel implements ActionListener {
         g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2);
     }
 
+    //Check if the current location is an apple, if so add to the snake and increase the game speed
     private void checkApple() {
 
         if ((x[0] == apple_x) && (y[0] == apple_y)) {
@@ -135,6 +176,22 @@ public class Board extends JPanel implements ActionListener {
         }
     }
 
+    // Check is user lands on a mine
+    private void checkMine() {
+        if ((x[0] == landmine1_x) && (y[0] == landmine1_y)) {
+            inGame = false;
+        } else if ((x[0] == landmine2_x) && (y[0] == landmine2_y)) {
+            inGame = false;
+        } else if ((x[0] == landmine3_x) && (y[0] == landmine3_y)) {
+            inGame = false;
+        } else if ((x[0] == landmine4_x) && (y[0] == landmine4_y)) {
+            inGame = false;
+        } else if ((x[0] == landmine5_x) && (y[0] == landmine5_y)) {
+            inGame = false;
+        }
+    }
+
+    //Update the location of each of the dots, depending on the direction add or increase dot size
     private void move() {
 
         for (int z = dots; z > 0; z--) {
@@ -159,6 +216,8 @@ public class Board extends JPanel implements ActionListener {
         }
     }
 
+    //If the snake eats itself, end the game
+    //Allow the snake to wrap over
     private void checkCollision() {
 
         for (int z = dots; z > 0; z--) {
@@ -183,27 +242,30 @@ public class Board extends JPanel implements ActionListener {
         if (x[0] < 0) {
             x[0] = B_WIDTH + x[0];
         }
-        
+
         if (!inGame) {
             timer.stop();
         }
     }
 
+    //Find a location for the next apple
     private void locateApple() {
-
-        int r = (int) (Math.random() * RAND_POS);
+        Random rand = new Random();
+        int r = rand.nextInt(60);
         apple_x = ((r * DOT_SIZE));
-
-        r = (int) (Math.random() * RAND_POS);
+        r = rand.nextInt(60);
         apple_y = ((r * DOT_SIZE));
     }
 
+    //Loop through these three options to play the game
+    //Check if spot is an apple, is a mine, is a collision, the move, the repaint the board
     @Override
     public void actionPerformed(ActionEvent e) {
 
         if (inGame) {
 
             checkApple();
+            checkMine();
             checkCollision();
             move();
         }
@@ -211,6 +273,72 @@ public class Board extends JPanel implements ActionListener {
         repaint();
     }
 
+    // Insert landmines on the window
+    // Check if landmine have a valid location
+    public void insertMines() {
+        Random random = new Random();
+        int xMine = random.nextInt(60);
+        landmine1_x = ((xMine * DOT_SIZE));
+        int yMine = random.nextInt(60);
+        landmine1_y = ((yMine * DOT_SIZE));
+
+        while ((landmine1_x == apple_x) && (landmine1_y == apple_y)) {
+            xMine = random.nextInt(60);
+            landmine1_x = ((xMine * DOT_SIZE));
+            yMine = random.nextInt(60);
+            landmine1_y = ((yMine * DOT_SIZE));
+        }
+
+        xMine = random.nextInt(60);
+        landmine2_x = ((xMine * DOT_SIZE));
+        yMine = random.nextInt(60);
+        landmine2_y = ((yMine * DOT_SIZE));
+
+        while ((landmine2_x == apple_x) && (landmine2_y == apple_y)) {
+            xMine = random.nextInt(60);
+            landmine2_x = ((xMine * DOT_SIZE));
+            yMine = random.nextInt(60);
+            landmine2_y = ((yMine * DOT_SIZE));
+        }
+
+        xMine = random.nextInt(60);
+        landmine3_x = ((xMine * DOT_SIZE));
+        yMine = random.nextInt(60);
+        landmine3_y = ((yMine * DOT_SIZE));
+
+        while ((landmine3_x == apple_x) && (landmine3_y == apple_y)) {
+            xMine = random.nextInt(60);
+            landmine3_x = ((xMine * DOT_SIZE));
+            yMine = random.nextInt(60);
+            landmine3_y = ((yMine * DOT_SIZE));
+        }
+
+        xMine = random.nextInt(60);
+        landmine4_x = ((xMine * DOT_SIZE));
+        yMine = random.nextInt(60);
+        landmine4_y = ((yMine * DOT_SIZE));
+
+        while ((landmine4_x == apple_x) && (landmine4_y == apple_y)) {
+            xMine = random.nextInt(60);
+            landmine4_x = ((xMine * DOT_SIZE));
+            yMine = random.nextInt(60);
+            landmine4_y = ((yMine * DOT_SIZE));
+        }
+
+        xMine = random.nextInt(60);
+        landmine5_x = ((xMine * DOT_SIZE));
+        yMine = random.nextInt(60);
+        landmine5_y = ((yMine * DOT_SIZE));
+
+        while ((landmine5_x == apple_x) && (landmine5_y == apple_y)) {
+            xMine = random.nextInt(60);
+            landmine5_x = ((xMine * DOT_SIZE));
+            yMine = random.nextInt(60);
+            landmine5_y = ((yMine * DOT_SIZE));
+        }
+    }
+
+    //This lets the user get keyboard input to move the snake
     private class TAdapter extends KeyAdapter {
 
         @Override
@@ -218,6 +346,7 @@ public class Board extends JPanel implements ActionListener {
 
             int key = e.getKeyCode();
 
+            //check the direction
             if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
                 leftDirection = true;
                 upDirection = false;
